b6c607a6e5a0cc617464d6d209e43967ef86b718
Hassan Ul Haq <81625175+Hassanulhaq52@users.noreply.github.com>
2022-07-15 18:14:16 +0500

    Storing Data into the Database Using Mongoose Model

--- Backend/routes/notes.js
+++ Backend/routes/notes.js
@@ -1,12 +1,9 @@
 const express = require('express');
-const router = express.Router(); 
+const router = express.Router();
 
-router.get('/', (req, res) => {
-
-
-
-res.json([]);
- 
-})
+router.get('/', (req, res)=>{
+    
+    res.json([])
+} )
 
 module.exports = router
